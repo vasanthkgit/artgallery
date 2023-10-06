3d8ca2f967a7f4148a319f1600d9138a8c9d3885
vasanth k <145307505+vasanthkgit@users.noreply.github.com>
2023-10-06 16:57:19 +0530

    Add files via upload

---
+++ agms/includes/footer.php
@@ -0,0 +1,59 @@
+<footer class="main-footer">
+      <div class="main-container">
+        <div class="main-footer__upper">
+          <div class="main-footer__row main-footer__row-1">
+            <h2 class="heading heading-sm main-footer__heading-sm">
+              <span>Social</span>
+            </h2>
+            <div class="main-footer__social-cont">
+              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/vasanthkln">
+                <img
+                  class="main-footer__icon"
+                  src="./assets/png/linkedin-ico.png"
+                  alt="icon"
+                />
+              </a>
+              <a target="_blank" rel="noreferrer" href="https://github.com/vasanthkgit">
+                <img
+                  class="main-footer__icon"
+                  src="./assets/png/github-ico.png"
+                  alt="icon"
+                />
+              </a>
+              <a target="_blank" rel="noreferrer" href="https://x.com/T_o_m________?t=xH0rsL0xeQHxcqx-oqHVUA&s=08">
+                <img
+                  class="main-footer__icon"
+                  src="./assets/png/twitter-ico.png"
+                  alt="icon"
+                />
+              </a>
+              <a target="_blank" rel="noreferrer" href="https://youtube.com/@immashup?feature=shared">
+                <img
+                  class="main-footer__icon"
+                  src="./assets/png/yt-ico.png"
+                  alt="icon"
+                />
+              </a>
+              <a target="_blank" rel="noreferrer" href="https://instagram.com/_vasan_th___?utm_source=qr&igshid=MzNlNGNkZWQ4Mg==">
+                <img
+                  class="main-footer__icon main-footer__icon--mr-none"
+                  src="./assets/png/insta-ico.png"
+                  alt="icon"
+                />
+              </a>
+            </div>
+          </div>
+          <div class="main-footer__row main-footer__row-2">
+            <h4 class="heading heading-sm text-lt">vasanth</h4>
+            <p class="main-footer__short-desc">
+              A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product
+            </p>
+          </div>
+        </div>
+
+       <div class="main-footer__lower">
+          &copy; Copyright 2023. Made by
+          <a rel="noreferrer" target="_blank" href=#>vasanth</a>
+        </div>
+      </div>
+    </footer>
